931d63e86ba8bb1633b5c4ec17ac09ec434db10d
harry708090 <emmyh626@gmail.com>
2026-08-19 13:20:10 -0700

    Fix Swagger

--- SchoolManagement/SchoolManagement.Api/Program.cs
+++ SchoolManagement/SchoolManagement.Api/Program.cs
@@ -18,11 +18,10 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
