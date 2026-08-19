3f95319c1f2e93cf0f7acf9080469ac5f90ec632
harry708090 <emmyh626@gmail.com>
2026-08-19 15:29:50 -0700

    Switch Database back to SQL Server

--- SchoolManagement/SchoolManagement.Api/Program.cs
+++ SchoolManagement/SchoolManagement.Api/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllers()
     });
 
 builder.Services.AddDbContext<SchoolDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
